c75ed1a8678b6f6c91a37e857b67ee5ebfbccee9
NaglaaAbdElkader <112434094+NaglaaAbdElkader@users.noreply.github.com>
2022-08-29 23:23:54 +0200

    Add files via upload

--- task_1/main.dart
+++ task_1/main.dart
@@ -1,1 +1,124 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.black87,
+        appBar: AppBar(
+          //title: const Text('Business Card'),
+          leading: Icon(Icons.favorite_border),
+          backgroundColor: Colors.black,
+          //foregroundColor: Colors.white,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                backgroundImage: AssetImage('images\girls.pjpg'),
+                radius: 100,
+              ),
+              Text(
+                'Naglaa Ibrahim',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 45,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'Software Engineer',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              ),
+              Text(
+                '@TwWorks',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(width: 10,),
+                  Icon(Icons.alternate_email_outlined,color: Colors.white,),
+                  SizedBox(width: 25,),
+                  Icon(Icons.phone,color: Colors.white,),
+                  SizedBox(width: 25,),
+                  Icon(Icons.mail,color: Colors.white,),
+
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        CircleAvatar(
+                          backgroundImage: AssetImage('images/night.PNG'),
+                          radius: 30,
+                        ),
+                        Text(
+                          '1.3K',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 45,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          'Followers',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ]),
+                  SizedBox(width: 50,),
+                  Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        CircleAvatar(
+                          backgroundImage: AssetImage('images/moon.PNG'),
+                          radius: 30,
+                        ),
+                        Text(
+                          '1.3K',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 45,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          'Followers',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ]),
+                  SizedBox(width: 10,),
+
+                ],
+              ),
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
