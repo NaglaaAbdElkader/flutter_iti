bf98bdfdf75912d8f52bf3ee330033f5e06f4acf
NaglaaAbdElkader <112434094+NaglaaAbdElkader@users.noreply.github.com>
2022-09-03 03:59:51 +0200

    Add files via upload

--- task_6/main.dart
+++ task_6/main.dart
@@ -1,1 +1,19 @@
+import 'package:flutter/material.dart';
+import 'home_screen.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "mini_Movie_app",
+      theme: ThemeData.dark(),
+      home: HomeScreen(),
+    );
+  }
+}
