2276ce2e215d2769b1f084d6ec0c00eac36f2085
NaglaaAbdElkader <112434094+NaglaaAbdElkader@users.noreply.github.com>
2022-09-07 12:33:47 +0200

    Add files via upload

--- task_7/main.dart
+++ task_7/main.dart
@@ -1,1 +1,34 @@
+import 'package:flutter/material.dart';
 
+import 'package:provider/provider.dart';
+
+import 'homeScreen.dart';
+import 'toDoList.dart';
+
+void main() {
+  runApp(ChangeNotifierProvider(
+    create: (context) => ToDoList(),
+    child:MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      darkTheme: ThemeData.dark(),
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
